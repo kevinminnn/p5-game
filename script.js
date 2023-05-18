5adf3436cd140b0fa01bc5af350ecfe0442123a0
kevinminnn <min123@uw.edu>
2023-05-17 23:48:06 -0700

    P5 Game

---
+++ script.js
@@ -0,0 +1,46 @@
+let xpos = 350;
+let ypos = 250;
+let dx = 5;
+let dy = 5;
+let isGameOver = false;
+
+function setup() {
+  createCanvas(750, 500);
+}
+
+function draw() {
+  background("black");
+
+  if (isGameOver) {
+    textAlign(CENTER);
+    textSize(32);
+    fill("white");
+    text("Game Over!", width / 2, height / 2);
+    return;
+  }
+
+  rect(10, constrain(ypos, 0, height - 100), 10, 100);
+  rect(730, constrain(mouseY, 0, height - 100), 10, 100);
+  ellipse(xpos, ypos, 20, 20);
+
+  if (xpos <= 20 && (ypos < mouseY || ypos > mouseY + 100)) {
+    isGameOver = true;
+    return;
+  }
+
+  if (xpos >= width - 20 && (ypos < constrain(mouseY, 0, height - 100) || ypos > constrain(mouseY, 0, height - 100) + 100)) {
+    isGameOver = true;
+    return;
+  }
+
+  xpos += dx;
+  ypos += dy;
+
+  if (xpos >= width - 20 || xpos <= 30) {
+    dx = -dx;
+  }
+
+  if (ypos >= height - 20 || ypos <= 20) {
+    dy = -dy;
+  }
+}
